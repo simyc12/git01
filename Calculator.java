d1670d4c9f15f4379d4999b23d052b8b180ec99b
java <java>
2017-02-02 16:14:20 +0900

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+
+	public Calculator() {
+	}	
+
+	public int add(int x, int y) {
+
+		return x + y;
+	}
+
+	public int substract(int x, int y) {
+
+		return x - y;
+	}
+}
--- Hello.java
+++ Hello.java
@@ -5,6 +5,11 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 
+		Calculator c = new Calculator();
+
+		System.out.println("3 + 4 = " + c.add(3,4));
+		System.out.println("3 - 4 = " + c.substract(3,4));
+
 	}
 
 }
